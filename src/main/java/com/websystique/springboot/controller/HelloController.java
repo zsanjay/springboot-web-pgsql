e021b9b9b4d4c4e9977166af5688d9c75f3bc6af
rejith <rejith@SUREJ2017>
2018-05-26 15:46:20 -0400

    381: Added nano

--- src/main/java/com/websystique/springboot/controller/HelloController.java
+++ src/main/java/com/websystique/springboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.websystique.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+	@Value("${nano:0}")
+	private String nano;
+	
 	@RequestMapping("/")
 	String home(ModelMap modal) {
 		modal.addAttribute("title", "Spring Boot Web");
-		modal.addAttribute("message", "NANO is 0\n" + System.getProperties());
+		modal.addAttribute("message", "NANO is " + nano);
 		return "hello";
 	}
 }
