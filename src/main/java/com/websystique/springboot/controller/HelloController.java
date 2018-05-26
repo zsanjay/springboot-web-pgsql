560e7ee5a05849a086e712f65d0686b6c73944a3
rejith <rejith@SUREJ2017>
2018-05-26 19:16:08 -0400

    Title is now customizable.

--- src/main/java/com/websystique/springboot/controller/HelloController.java
+++ src/main/java/com/websystique/springboot/controller/HelloController.java
@@ -11,12 +11,15 @@ public class HelloController {
 	@Value("${appz.image_version:0.0.0}")
 	private String version;
 	
+	@Value("${title:0}")
+	private String title;
+
 	@Value("${nano:0}")
 	private String nano;
 	
 	@RequestMapping("/")
 	String home(ModelMap modal) {
-		modal.addAttribute("title", "Spring Boot Web " + version);
+		modal.addAttribute("title", title + " " + version);
 		modal.addAttribute("message", "NANO is " + nano);
 		return "hello";
 	}
