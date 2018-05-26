0c84af09abc1bfd0f3f4fb7356c2a82271b2e1a0
rejith <rejith@SUREJ2017>
2018-05-26 15:51:00 -0400

    381: Displaying version.

--- src/main/java/com/websystique/springboot/controller/HelloController.java
+++ src/main/java/com/websystique/springboot/controller/HelloController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+	@Value("${appz.image_version:0.0.0}")
+	private String version;
+	
 	@Value("${nano:0}")
 	private String nano;
 	
 	@RequestMapping("/")
 	String home(ModelMap modal) {
-		modal.addAttribute("title", "Spring Boot Web");
+		modal.addAttribute("title", "Spring Boot Web " + version);
 		modal.addAttribute("message", "NANO is " + nano);
 		return "hello";
 	}
