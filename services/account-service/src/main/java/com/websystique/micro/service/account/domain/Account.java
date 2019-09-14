7e46d673d16c2aa3af995b70356b88e70f79a917
Evseev VD <evseevvd@sib-soft.ru>
2019-09-14 23:09:43 +0700

    create a new account micro service  - Account service  - Rest API

---
+++ services/account-service/src/main/java/com/websystique/micro/service/account/domain/Account.java
@@ -0,0 +1,54 @@
+package com.websystique.micro.service.account.domain;
+
+public class Account {
+
+    private String id;
+
+    private String login;
+
+    private String email;
+
+    private String name;
+
+    private String fullName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
