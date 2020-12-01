81bde0539badf639788112ed61695a3d6c848d49
Sanjeev Kumar <49405996+skumarpi@users.noreply.github.com>
2020-12-01 16:52:15 -0500

    Update create_account_model.sql

--- ddl/create_account_model.sql
+++ ddl/create_account_model.sql
@@ -5,15 +5,15 @@ CREATE TABLE ACCOUNT (
     LOGIN VARCHAR(50),
     email VARCHAR(50),
     PRIMARY KEY (ID)
-)
+);
 
-INSERT INTO ACCOUNT(1, 'Devid', 'Devid Brain', 'dbrain', 'dbrain@mail.com');
-INSERT INTO ACCOUNT(2, 'Mikle', 'Mikle Force', 'force', 'force1@mail.com');
-INSERT INTO ACCOUNT(3, 'Alisha', 'Alisha Brain', 'ali', 'Alisha@mail.com');
-INSERT INTO ACCOUNT(4, 'Kris', 'Kris Lyn', 'lyn', 'lyn@mail.com');
-INSERT INTO ACCOUNT(5, 'Marcus', 'Marcus Mo', 'marcusMo', 'mmo@mail.com');
-INSERT INTO ACCOUNT(6, 'Trish', 'Trish Devidson', 'tdev', 'tdev@mail.com');
-INSERT INTO ACCOUNT(7, 'Ronny', 'Ronny Fox', 'rnnf', 'rnnf@mail.com');
-INSERT INTO ACCOUNT(8, 'Amber', 'Amber Scott', 'asct', 'asct@mail.com');
-INSERT INTO ACCOUNT(9, 'Alain', 'Alain White', 'alain', 'alain@mail.com');
-INSERT INTO ACCOUNT(10, 'Kira', 'Kira Loft', 'kloft', 'loft@mail.com');
+INSERT INTO ACCOUNT Values(1, 'Devid', 'Devid Brain', 'dbrain', 'dbrain@mail.com');
+INSERT INTO ACCOUNT Values(2, 'Mikle', 'Mikle Force', 'force', 'force1@mail.com');
+INSERT INTO ACCOUNT Values(3, 'Alisha', 'Alisha Brain', 'ali', 'Alisha@mail.com');
+INSERT INTO ACCOUNT Values(4, 'Kris', 'Kris Lyn', 'lyn', 'lyn@mail.com');
+INSERT INTO ACCOUNT Values(5, 'Marcus', 'Marcus Mo', 'marcusMo', 'mmo@mail.com');
+INSERT INTO ACCOUNT Values(6, 'Trish', 'Trish Devidson', 'tdev', 'tdev@mail.com');
+INSERT INTO ACCOUNT Values(7, 'Ronny', 'Ronny Fox', 'rnnf', 'rnnf@mail.com');
+INSERT INTO ACCOUNT Values(8, 'Amber', 'Amber Scott', 'asct', 'asct@mail.com');
+INSERT INTO ACCOUNT Values(9, 'Alain', 'Alain White', 'alain', 'alain@mail.com');
+INSERT INTO ACCOUNT Values(10, 'Kira', 'Kira Loft', 'kloft', 'loft@mail.com');
